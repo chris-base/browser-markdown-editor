1f6e64390919d0dbf1bc6fb6c0881c46403454db
chris-base <baseappsdev@gmail.com>
2022-07-03 17:39:11 -0500

    started edit file component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import HeaderComponent from "./Components/HeaderComponent";
+import EditFileComponent from "./Components/EditFileComponent";
 
 function App() {
   return (
     <div className='App'>
       <HeaderComponent />
+
+      <div id='editFilePreviewContainer'>
+        <EditFileComponent />
+      </div>
     </div>
   );
 }
---
+++ src/Components/EditFileComponent.js
@@ -0,0 +1,13 @@
+import "../Styles/EditFileStyles.css";
+
+const EditFileComponent = () => {
+  return (
+    <div id='editFileContainer'>
+      <div id='editFileHeader'>
+        <p id='editFileHeaderText'>MARKDOWN</p>
+      </div>
+    </div>
+  );
+};
+
+export default EditFileComponent;
